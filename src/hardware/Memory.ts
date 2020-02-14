b8260bdf7a6303c04d731a6fa007e02850633f60
kennymejia <kenny.mejia1@marist.edu>
2020-02-14 12:05:33 -0500

    Lab 2 Milestone 1 Complete

--- src/hardware/Memory.ts
+++ src/hardware/Memory.ts
@@ -11,25 +11,45 @@ export class Memory extends Hardware implements ClockListener{
 
     constructor() {
         super(0, "RAM");
-        this.log("Memory Created");
-        this.mar = 0;
-        this.mdr = 0;
+        this.mar = 0x0000;
+        this.mdr = 0x00;
+        this.memory = new Array (65536);
         this.isExecuting = false;
-
+        this.log("Memory Created - Addressable Space: " + this.memory.length);
     }
 
-    public mar: number;
-    public mdr: number;
-    public isExecuting: boolean;
+    private mar: number;
+    private mdr: number;
+    private memory: Array<number>;
+    private isExecuting: boolean;
 
     public reset(): void {
-        this.mar = 0;
-        this.mdr = 0;
+        this.mar = 0x0000;
+        this.mdr = 0x00;
         this.isExecuting = false;
+
+        for (let address of this.memory) {
+            this.memory[address] = 0x00;
+        }
+
     }
 
     public pulse(): void {
 
         this.log("Received Clock Pulse");
+    }
+
+    public read (): void {
+        
+        this.mdr = this.memory[this.mar];
+        
+        this.log("Address Received Reading Data")
+    }
+
+    public write (): void {
+
+        this.memory[this.mar] = this.mdr;
+
+        this.log("Data Received Setting Now")
     }
 }
